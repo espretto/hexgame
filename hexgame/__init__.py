73d0e142ed53de5964ee8687cd206de7054fbd6e
espretto <espretto@gmail.com>
2017-10-12 23:19:32 +0200

    [fix] change order of coordinates : board[x][y] is the new black [add] neighbours convention, valid play and finished board detection

--- hexgame/__init__.py
+++ hexgame/__init__.py
@@ -1,8 +1,9 @@
 
 import random
 
-VERTICAL = -1
-HORIZONTAL = 1
+EMPTY = 0
+HORIZ = 1
+VERTI = -1
 
 class AbstractPlayer (object):
 
@@ -10,18 +11,83 @@ class AbstractPlayer (object):
         raise NotImplementedError()
 
 
+class Tile (object):
+
+    def __init__ (self, x, y):
+        self.x = x
+        self.y = y
+        self.z = x+y
+
+    def __eq__ (self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__ (self, other):
+        return not self.__eq__(other)
+
+
 class Game (object):
 
     def __init__ (self, cols=11, rows=11):
-        self.board = [[0 for col in range(cols)] for row in range(rows)]
+        self.cols = cols
+        self.rows = rows
+        self.board = [[0 for row in range(rows)] for col in range(cols)]
 
     def play (self, alice, bob):
-          
-        
+        raise NotImplementedError()
 
-        if random.random() > 0.5:
-            alice.play(self.board, VERTICAL)
-            bob.play(self.board, HORIZONTAL)
-        else:
-            bob.play(self.board, VERTICAL)
-            alice.play(self.board, HORIZONTAL)
+    def isValid (self, tile):
+        return self.contains(tile) and self.board[tile.x][tile.y] == EMPTY
+
+    def contains (self, tile):
+        return 0 <= tile.x < self.cols and 0 <= tile.y < self.rows
+
+    def neighbours (self, tile):
+
+        neighbours = [
+            Tile(tile.x+1, tile.y  ),
+            Tile(tile.x-1, tile.y  ),
+            Tile(tile.x+1, tile.y-1),
+            Tile(tile.x-1, tile.y+1),
+            Tile(tile.x,   tile.y-1),
+            Tile(tile.x,   tile.y+1)
+        ]
+
+        return [tile for tile in neighbours if self.contains(tile)]
+
+    def isFinished (self, direction):
+        
+        if direction == HORIZ:
+            x = self.cols-1
+            origins = [Tile(0, y) for y in range(self.rows) if self.board[0][y] == direction]
+            destins = [Tile(x, y) for y in range(self.rows) if self.board[x][y] == direction]
+        elif direction == VERTI:
+            y = self.rows-1
+            origins = [Tile(x, 0) for x in range(self.cols) if self.board[x][0] == direction]
+            destins = [Tile(x, y) for x in range(self.cols) if self.board[x][y] == direction]
+
+        if not origins or not destins:
+            return False
+
+        # breath-first search for a connection between two opposite borders
+        todo = set()
+        done = set()
+
+        for origin in origins:
+            todo.clear()
+            done.clear()
+
+            todo.add(origin)
+
+            while todo:
+                tile = todo.pop()
+
+                if tile in destins:
+                    return True
+                
+                done.add(tile)
+
+                for neighbour in self.neighbours(tile):
+                    if neighbour not in done:
+                        todo.add(neighbour)
+
+        return False
